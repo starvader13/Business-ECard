421370dea04759044200de8eeb81ae572f145e06
starvader13 <ashutoshgupta1311@gmail.com>
2024-04-19 11:07:10 +0530

    Deploy frontend
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',  // Specify the output directory for build files
+    outDir: 'dist',  // Specify the output directory for build files
     sourcemap: true  // Generate source maps for debugging
   }
 });
